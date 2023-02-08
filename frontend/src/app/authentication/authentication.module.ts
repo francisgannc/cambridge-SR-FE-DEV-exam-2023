7ecd0b2cd9079cfd8730da451931b123cabd981e
francisgannc <francisgannworkemail@gmail.com>
2023-02-08 20:34:46 +0800

    initial login component and route

--- frontend/src/app/authentication/authentication.module.ts
+++ frontend/src/app/authentication/authentication.module.ts
@@ -1,17 +1,12 @@
+import { AngularMaterialModule } from './../shared/angular-material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponent } from './login/login.component';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthenticationRoutingModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, AuthenticationRoutingModule, AngularMaterialModule],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
